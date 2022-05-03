a12abfd0ffd0241b31d83f2d548192694cc2a9e7
devskygarden123 <104509593+devskygarden123@users.noreply.github.com>
2022-05-03 16:35:34 +0700

    casino

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb+srv://cluster0.nftfv.mongodb.net/test',
+	'options': {
+		//'user':   'x29sky',
+		//'pass':   'anhyeu123',
+		'dbName': 'X29', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
